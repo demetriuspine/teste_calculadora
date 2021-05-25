1a9128ab4889df8fb87a645b04925687c79fa900
lucas <mr.pine97@gmail.com>
2021-05-25 13:27:49 -0300

    faz soma e subtração digitados pelo usuário

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,10 @@
 a = int(input('digite o primeiro valor'))
 b = int(input('digite o segundo valor'))
-resultado = a + b
+operacao = input('Digite a operação (+ ou -): ')
+if operacao == '+':
+    resultado = a + b
+elif operacao == '-':
+    resultado = a - b
+else:
+    resultado = 'Operação inválida'
 print(resultado)
